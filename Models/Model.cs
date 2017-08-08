be8eb0fcc90116e7ae588fa9145d7308fb85cc69
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-08-08 16:57:13 -0500

    ApplyConstraints

---
+++ Models/Model.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace vega.Models
+{
+    [Table("Models")]
+    public class Model
+    {        
+        [Required]
+        [StringLength(255)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Make Make { get; set; }
+
+        // forgein key property
+        public int MakeId { get; set; }
+    }
+
+}
