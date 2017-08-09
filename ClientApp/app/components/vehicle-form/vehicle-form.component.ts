d58e67362a355d36d8733300a448fc177517b0c1
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-08-09 05:47:50 -0500

    Add features to Model, Controller, and View

---
+++ ClientApp/app/components/vehicle-form/vehicle-form.component.ts
@@ -0,0 +1,39 @@
+import { FeatureService } from './../../services/feature.service';
+import { MakeService } from './../../services/make.service';
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-form',
+  templateUrl: './vehicle-form.component.html',
+  styleUrls: ['./vehicle-form.component.css']
+})
+export class VehicleFormComponent implements OnInit {
+  makes: any[];
+  models: any[];
+  features: any[];
+  vehicle: any = {};
+
+
+  constructor(
+    private makeService: MakeService,
+    private featureService: FeatureService ) { }
+
+  ngOnInit() {
+    this.makeService.getMakes().subscribe(makes => 
+    
+    this.makes = makes);
+
+    this.featureService.getFeatures().subscribe(features => 
+      this.features = features);
+    
+
+  }
+
+  onMakeChange() {
+    // console.log("VEHICOLE", this.vehicle);
+    var selectedMake = this.makes.find(m => m.id == this.vehicle.make);
+    this.models = selectedMake ? selectedMake.models : [];
+  }
+
+}
--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -12,6 +12,7 @@ namespace vega.Persistence
         }
 
         public DbSet<Make> Makes {get; set;}
-        
+        public DbSet<Feature> Features { get; set; }
+
     }
 }
