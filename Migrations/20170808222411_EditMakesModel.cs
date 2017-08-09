bf14e2c0c4d07cc93419babc56b67bc602417777
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-08-09 03:05:49 -0500

    Add Automapper

---
+++ Migrations/20170808222411_EditMakesModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vega.Migrations
+{
+    public partial class EditMakesModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Makes",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Makes",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
--- Models/Make.cs
+++ Models/Make.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace vega.Models
 {
     public class Make
     {
+        [Required]
+        [StringLength(255)]
         public int Id { get; set; }
-        public int Name { get; set; }
+        public string Name { get; set; }
         public  ICollection<Model> Models {get; set; }
 
         public Make()
