804caf4baacd584939f9e77b740594dd3a0ed65e
Hien Le <hienle@Hiens-MacBook-Pro.local>
2017-08-09 20:10:46 -0500

    Implement validation.

--- Controllers/Resources/VehicleResource.cs
+++ Controllers/Resources/VehicleResource.cs
@@ -1,6 +1,8 @@
+
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using vega.Models;
 
 namespace vega.Controllers.Resources
@@ -11,6 +13,7 @@ namespace vega.Controllers.Resources
         public int ModelId { get; set; }
         public bool IsRegistered { get; set; }
 
+        [Required]
         public ContactResource Contact { get; set; }
         public ICollection<int> Features { get; set; }
 
--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -1,3 +1,4 @@
+
 using Microsoft.EntityFrameworkCore;
 using vega.Models;
 
@@ -5,7 +6,9 @@ namespace vega.Persistence
 {
     public class VegaDbContext : DbContext
     {
+        public DbSet<Vehicle> Vehicles { get; set; }
         public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
         public DbSet<Feature> Features { get; set; }
 
         public VegaDbContext(DbContextOptions<VegaDbContext> options) 
